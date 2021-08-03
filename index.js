9661de34ca6d5a724c1fa15d93fab37eeb530d7f
Manu Web Dev <manuelmartindev@outlook.com>
2021-08-03 17:13:00 +0200

    updated env of example

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const { config } = require("dotenv");
+
 const { indexRouter } = require("./routes/main");
+
+config();
+
 const app = express();
 
 app.set("PORT", 4000 | process.env.PORT);
